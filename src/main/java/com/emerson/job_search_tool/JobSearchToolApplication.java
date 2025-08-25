43c1975fecfeded4d2d1d0fad0c4ce65f2f5c86d
Emerson Leite <emersonleite@galileosoft.com.br>
2025-08-25 14:59:12 -0300

    first commit

---
+++ src/main/java/com/emerson/job_search_tool/JobSearchToolApplication.java
@@ -0,0 +1,13 @@
+package com.emerson.job_search_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emerson/job_search_tool/JobSearchToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerson.job_search_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
