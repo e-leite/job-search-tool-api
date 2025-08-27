954810bb980c177934772aab2c064a5cfa385553
Emerson Leite <emersonleite@galileosoft.com.br>
2025-08-27 10:50:58 -0300

    feat: add method fromDescription.

--- src/main/java/com/emerson/job_search_tool/enums/ProgramingLanguageCategory.java
+++ src/main/java/com/emerson/job_search_tool/enums/ProgramingLanguageCategory.java
@@ -1,5 +1,7 @@
 package com.emerson.job_search_tool.enums;
 
+import java.util.stream.Stream;
+
 public enum ProgramingLanguageCategory {
     FRONT_END("Front-End"),
     BACK_END("Back-End");
@@ -13,5 +15,13 @@ public enum ProgramingLanguageCategory {
     @Override
     public String toString() {
         return this.description;
+    }
+
+    public static ProgramingLanguageCategory fromDescription(String description) {
+        return Stream.of(ProgramingLanguageCategory.values())
+            .filter(c -> c.toString().equals(description))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Programing language category " + description + " invalid."));
+
     }
 }
