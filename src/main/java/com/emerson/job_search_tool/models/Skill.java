a6102352df2502bf9333852f665ba9023120446a
Emerson Leite <emersonleite@galileosoft.com.br>
2025-08-30 19:42:01 -0300

    feat: create Skill.

---
+++ src/main/java/com/emerson/job_search_tool/models/Skill.java
@@ -0,0 +1,95 @@
+package com.emerson.job_search_tool.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.emerson.job_search_tool.commons.Auditable;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "skills")
+public class Skill extends Auditable implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Column(name = "name", length = 120, nullable = false, unique = true)
+    private String name;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "skillcategories_id")
+    private SkillCategory skillCategory;
+    
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public Skill() {
+    }
+
+    public Skill(UUID id, @NotBlank(message = "Name is mandatory") String name, SkillCategory skillCategory,
+            LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.skillCategory = skillCategory;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SkillCategory getCategory() {
+        return skillCategory;
+    }
+
+    public void setCategory(SkillCategory skillCategory) {
+        this.skillCategory = skillCategory;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }    
+}
