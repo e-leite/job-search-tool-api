eca3b83ede7729e2d452106bb5c38497da4c278b
Emerson Leite <emersonleite@galileosoft.com.br>
2025-09-08 07:34:01 -0300

    feat: create Enterprise.

---
+++ src/main/java/com/emerson/job_search_tool/models/Enterprise.java
@@ -0,0 +1,136 @@
+package com.emerson.job_search_tool.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.emerson.job_search_tool.commons.Auditable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_enterprise")
+public class Enterprise extends Auditable implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "Name is mandatory.")
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "overview")
+    private String overview;
+
+    @Column(name = "site")
+    private String site;
+
+    @Column(name = "industry")
+    private String industry;
+
+    @Column(name = "company_size")
+    private String companySize;
+
+    @Column(name = "foundation_year")
+    private Integer foundationYear;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public Enterprise() {
+    }
+
+    public Enterprise(UUID id, @NotBlank(message = "Name is mandatory.") String name, String overview, String site,
+            String industry, String companySize, Integer foundationYear) {
+        this.id = id;
+        this.name = name;
+        this.overview = overview;
+        this.site = site;
+        this.industry = industry;
+        this.companySize = companySize;
+        this.foundationYear = foundationYear;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getCompanySize() {
+        return companySize;
+    }
+
+    public void setCompanySize(String companySize) {
+        this.companySize = companySize;
+    }
+
+    public Integer getFoundationYear() {
+        return foundationYear;
+    }
+
+    public void setFoundationYear(Integer foundationYear) {
+        this.foundationYear = foundationYear;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    
+}
